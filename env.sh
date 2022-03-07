02f911e2aeaf7ef88fbf0a947f28de826f641036
Karthik Balu <kbalu@qti.qualcomm.com>
2022-03-07 14:19:37 -0800

    Syncing...

--- env.sh
+++ env.sh
@@ -7,7 +7,7 @@ export GITHUB_HOME=$GITHUB/balukarthik
 export GITHUB_WORK=$GITHUB/kbalu
 
 # Notes directory
-export NOTES=$GITHUB_HOME/notes
+export NOTES=$GITHUB_HOME/Notes
 
 # Vim is my editor
 export VISUAL=vim
