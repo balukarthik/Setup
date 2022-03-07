371a20598ea91c9058a3f9dbb6aaef01c33ffe7e
Karthik Balu <kbalu@qti.qualcomm.com>
2022-03-07 12:28:53 -0800

    updated env.sh

--- env.sh
+++ env.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+
+# Github directory
 export GITHUB=$HOME/github
+
+# Notes directory
 export NOTES=$HOME/github/balukarthik/notes
+
+# Vim is my editor
+export VISUAL=vim
+export EDITOR="$VISUAL"
