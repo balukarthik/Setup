d9c92e4615a1cfffa0e897a5d21a3129f1570103
Karthik Balu <kbalu@qti.qualcomm.com>
2022-03-07 17:39:59 -0800

    Adding todo.sh

---
+++ todo.txt-cli/tests/t1200-pri.sh
@@ -0,0 +1,138 @@
+#!/bin/bash
+
+test_description='basic priority functionality
+'
+. ./test-lib.sh
+
+test_todo_session 'priority usage' <<EOF
+>>> todo.sh pri B B
+usage: todo.sh pri ITEM# PRIORITY[, ITEM# PRIORITY, ...]
+note: PRIORITY must be anywhere from A to Z.
+=== 1
+EOF
+
+cat > todo.txt <<EOF
+smell the uppercase Roses +flowers @outside
+notice the sunflowers
+stop
+EOF
+test_todo_session 'basic priority' <<EOF
+>>> todo.sh list
+2 notice the sunflowers
+1 smell the uppercase Roses +flowers @outside
+3 stop
+--
+TODO: 3 of 3 tasks shown
+
+>>> todo.sh pri 1 B
+1 (B) smell the uppercase Roses +flowers @outside
+TODO: 1 prioritized (B).
+
+>>> todo.sh list
+[0;32m1 (B) smell the uppercase Roses +flowers @outside[0m
+2 notice the sunflowers
+3 stop
+--
+TODO: 3 of 3 tasks shown
+
+>>> todo.sh -p list
+1 (B) smell the uppercase Roses +flowers @outside
+2 notice the sunflowers
+3 stop
+--
+TODO: 3 of 3 tasks shown
+
+>>> todo.sh pri 2 C
+2 (C) notice the sunflowers
+TODO: 2 prioritized (C).
+
+>>> todo.sh -p list
+1 (B) smell the uppercase Roses +flowers @outside
+2 (C) notice the sunflowers
+3 stop
+--
+TODO: 3 of 3 tasks shown
+
+>>> todo.sh add "smell the coffee +wakeup"
+4 smell the coffee +wakeup
+TODO: 4 added.
+
+>>> todo.sh -p list
+1 (B) smell the uppercase Roses +flowers @outside
+2 (C) notice the sunflowers
+4 smell the coffee +wakeup
+3 stop
+--
+TODO: 4 of 4 tasks shown
+EOF
+
+test_todo_session 'priority error' <<EOF
+>>> todo.sh pri 10 B
+=== 1
+TODO: No task 10.
+EOF
+
+cat > todo.txt <<EOF
+(B) smell the uppercase Roses +flowers @outside
+(C) notice the sunflowers
+stop
+EOF
+test_todo_session 'reprioritize' <<EOF
+>>> todo.sh pri 2 A
+2 (A) notice the sunflowers
+TODO: 2 re-prioritized from (C) to (A).
+
+>>> todo.sh -p list
+2 (A) notice the sunflowers
+1 (B) smell the uppercase Roses +flowers @outside
+3 stop
+--
+TODO: 3 of 3 tasks shown
+
+>>> todo.sh pri 2 a
+2 (A) notice the sunflowers
+TODO: 2 already prioritized (A).
+
+>>> todo.sh -p list
+2 (A) notice the sunflowers
+1 (B) smell the uppercase Roses +flowers @outside
+3 stop
+--
+TODO: 3 of 3 tasks shown
+EOF
+
+cat > todo.txt <<EOF
+smell the uppercase Roses +flowers @outside
+notice the sunflowers
+stop
+EOF
+test_todo_session 'multiple priority' <<EOF
+>>> todo.sh pri 1 A 2 B
+1 (A) smell the uppercase Roses +flowers @outside
+TODO: 1 prioritized (A).
+2 (B) notice the sunflowers
+TODO: 2 prioritized (B).
+EOF
+
+test_todo_session 'multiple reprioritize' <<EOF
+>>> todo.sh pri 1 Z 2 X
+1 (Z) smell the uppercase Roses +flowers @outside
+TODO: 1 re-prioritized from (A) to (Z).
+2 (X) notice the sunflowers
+TODO: 2 re-prioritized from (B) to (X).
+EOF
+
+test_todo_session 'multiple prioritize error' <<EOF
+>>> todo.sh pri 1 B 4 B
+=== 1
+1 (B) smell the uppercase Roses +flowers @outside
+TODO: 1 re-prioritized from (Z) to (B).
+TODO: No task 4.
+
+>>> todo.sh pri 1 C 4 B 3 A
+=== 1
+1 (C) smell the uppercase Roses +flowers @outside
+TODO: 1 re-prioritized from (B) to (C).
+TODO: No task 4.
+EOF
+test_done
---
+++ todo.txt-cli/tests/t1310-listcon.sh
@@ -0,0 +1,127 @@
+#!/bin/bash
+
+test_description='listcon functionality
+
+This test checks basic context listing functionality
+'
+. ./test-lib.sh
+
+cat > todo.txt <<EOF
+item 1
+item 2
+item 3
+EOF
+test_expect_success 'listcon no contexts' '
+    todo.sh listcon > output && ! test -s output
+'
+
+cat > todo.txt <<EOF
+(A) @1 -- Some context 1 task, whitespace, one char
+(A) @c2 -- Some context 2 task, whitespace, two char
+@con03 -- Some context 3 task, no whitespace
+@con04 -- Some context 4 task, no whitespace
+@con05@con06 -- weird context
+EOF
+test_todo_session 'Single context per line' <<EOF
+>>> todo.sh listcon
+@1
+@c2
+@con03
+@con04
+@con05@con06
+EOF
+
+cat > todo.txt <<EOF
+@con01 -- Some context 1 task
+@con02 -- Some context 2 task
+@con02 @con03 -- Multi-context task
+EOF
+test_todo_session 'Multi-context per line' <<EOF
+>>> todo.sh listcon
+@con01
+@con02
+@con03
+EOF
+
+cat > todo.txt <<EOF
+@con01 -- Some context 1 task
+@con02 -- Some context 2 task
+@con02 ginatrapani@gmail.com -- Some context 2 task
+EOF
+test_todo_session 'listcon e-mail address test' <<EOF
+>>> todo.sh listcon
+@con01
+@con02
+EOF
+
+cat > todo.txt <<EOF
+(B) smell the uppercase Roses +roses @outside +shared
+(C) notice the sunflowers +sunflowers @garden +shared +landscape
+stop
+EOF
+test_todo_session 'listcon with project' <<EOF
+>>> todo.sh listcon +landscape
+@garden
+EOF
+
+cat > todo.txt <<EOF
+(B) +math (@school or @home) integrate @x and @y
+(C) say thanks @GinaTrapani w:@OtherContributors
+stop
+EOF
+test_todo_session 'listcon with default configuration' <<EOF
+>>> todo.sh listcon
+@GinaTrapani
+@home)
+@x
+@y
+EOF
+test_todo_session 'listcon limiting to multi-character sequences' <<EOF
+>>> TODOTXT_SIGIL_VALID_PATTERN='.\{2,\}' todo.sh listcon
+@GinaTrapani
+@home)
+EOF
+test_todo_session 'listcon allowing w: marker before contexts' <<EOF
+>>> TODOTXT_SIGIL_BEFORE_PATTERN='\(w:\)\{0,1\}' todo.sh listcon
+@GinaTrapani
+@OtherContributors
+@home)
+@x
+@y
+EOF
+test_todo_session 'listcon allowing parentheses around contexts' <<EOF
+>>> TODOTXT_SIGIL_BEFORE_PATTERN='(\{0,1\}' TODOTXT_SIGIL_AFTER_PATTERN=')\{0,1\}' todo.sh listcon
+@GinaTrapani
+@home
+@school
+@x
+@y
+EOF
+test_todo_session 'listcon with all customizations combined' <<EOF
+>>> TODOTXT_SIGIL_VALID_PATTERN='.\{2,\}' TODOTXT_SIGIL_BEFORE_PATTERN='\(w:\)\{0,1\}\((\)\{0,1\}' TODOTXT_SIGIL_AFTER_PATTERN=')\{0,1\}' todo.sh listcon
+@GinaTrapani
+@OtherContributors
+@home
+@school
+EOF
+
+cat > todo.txt <<EOF
+@con01 -- Some context 1 task
+EOF
+cat > done.txt <<EOF
+x 2012-02-21 @done01 -- Some context 1 done task
+x 2012-02-21 @done02 -- Some context 2 done task
+EOF
+test_todo_session 'listcon from done tasks' <<'EOF'
+>>> TODOTXT_SOURCEVAR=\$DONE_FILE todo.sh listcon
+@done01
+@done02
+EOF
+test_todo_session 'listcon from combined open + done tasks' <<'EOF'
+>>> TODOTXT_SOURCEVAR='("$TODO_FILE" "$DONE_FILE")' todo.sh listcon
+@con01
+@done01
+@done02
+EOF
+
+test_done
