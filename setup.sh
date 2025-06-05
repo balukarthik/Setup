10998aa7249c5a4a2ea0327371951aef6a9e7277
Karthik Balu <balukarthik@gmail.com>
2025-06-05 07:43:26 -0700

    Syncing...

--- setup.sh
+++ setup.sh
@@ -124,17 +124,4 @@ else
     :
 fi
 
-# Install node.js
-# Download and install fnm:
-curl -o- https://fnm.vercel.app/install | bash
-
-# Download and install Node.js:
-fnm install 22
-
-# Verify the Node.js version:
-node -v 
-
-# Verify npm version:
-npm -v 
-
 echo "Setup Complete" 
