de89e3b6e3a829376276e3981462659b4e9dd554
Karthik Balu <balukarthik@gmail.com>
2025-06-08 14:21:55 -0700

    Syncing...

---
+++ env.ps1
@@ -0,0 +1,11 @@
+# Github directory
+$env:GITHUB = "$HOME\github"
+$env:GITHUB_HOME = "$env:GITHUB\balukarthik"
+
+# Notes directory
+$env:NOTES = "$env:GITHUB_HOME\Notes"
+
+# Vim is my editor
+$env:VISUAL = "vim"
+$env:EDITOR = $env:VISUAL
+
