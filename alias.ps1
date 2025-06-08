5caa9551d823fe5d9a84ffaf57f4d484de729e49
balukarthik <balukarthik@gmail.com>
2025-06-08 15:15:48 -0700

    Syncing...

--- alias.ps1
+++ alias.ps1
@@ -1,5 +1,7 @@
 # Alias for sync-one.ps1 (assumes sync-one.sh is in $HOME\bin and executable)
 Set-Alias s "$HOME\bin\sync-one.ps1"
+Set-Alias vim 'C:\Program Files (x86)\Vim\vim91\vim.exe'
+Set-Alias vi 'C:\Program Files (x86)\Vim\vim91\vim.exe'
 
 # Calculator function (uses PowerShell's Invoke-Expression for math)
 function c {
