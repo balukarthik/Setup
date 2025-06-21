18c69c5d04d9cb59217ac448baeb362b332bea73
Karthik Balu <balukarthik@gmail.com>
2025-06-21 16:10:12 -0700

    Syncing...

--- alias.sh
+++ alias.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 alias s="sync-one.sh"
+alias vi="vim"
 
-#Technically not an alias but I need a calculator  
+#Technically not an alias but I need a calculator
 c() { printf "%s\n" "$@" | bc -l; }
 
 
