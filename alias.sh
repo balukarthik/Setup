64e0b34d4ea8b8f55bcd19320094af569fa0d9f0
Karthik Balu <balukarthik@gmail.com>
2026-01-19 08:46:25 -0800

    Syncing...

--- alias.sh
+++ alias.sh
@@ -3,7 +3,7 @@
 alias s="sync-one.sh"
 alias vi="vim"
 alias t="todo.sh"
-alias gls="git ls-tree --name-only HEAD -z | TZ=UTC xargs -0n1 -I_ git --no-pager log -1 --date=iso-local --format=\"%ad _\" -- _ | sort"
+alias gls="git ls-tree --name-only HEAD -z | TZ=UTC xargs -0 -I_ git --no-pager log -1 --date=iso-local --format=\"%ad _\" -- _ | sort"
 
 #Technically not an alias but I need a calculator
 c() { printf "%s\n" "$@" | bc -l; }
