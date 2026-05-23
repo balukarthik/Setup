0e5b93a3103afbc16f8e5565caf901aef746813d
Karthik Balu <kbalu@qti.qualcomm.com>
2026-05-23 00:15:26 -0700

    Syncing...

--- alias.sh
+++ alias.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 alias s="sync-one.sh"
+alias gls='git ls-tree --name-only HEAD trackers/ -z | TZ=UTC xargs -0n1 -I_ git --no-pager log -1 --date=iso-local --format="%ad _" -- _ | sort'
 
 #Technically not an alias but I need a calculator  
 c() { printf "%s\n" "$@" | bc -l; }
